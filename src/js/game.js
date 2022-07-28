d10a00fa738de8ce2facaeb56219080e989d38ec
Amit Purohit <a.purohit2@zensar.com>
2022-07-28 09:56:51 +0530

    responsiveness fixed

--- src/js/game.js
+++ src/js/game.js
@@ -5,23 +5,20 @@ var canvas,
     context, 
     brickWidth = 117,
     brickHeight = 35,
-    maxMobile = window.matchMedia('(max-width: 768px)'),
-    minTablet = window.matchMedia('(min-width: 769px)'),
-    maxTablet = window.matchMedia('(max-width: 1024px)'),
-    minDesktop = window.matchMedia('(min-width: 1024px)'),
-    maxDesktop = window.matchMedia('(max-width: 1440px)');
-    
+    maxMob = window.matchMedia('(max-width: 768px)'),
+    minTab = window.matchMedia('(min-width: 769px)'),
+    maxTab = window.matchMedia('(max-width: 1024px)'),
+    minLap = window.matchMedia('(min-width: 1024px)'),
+    minLap1 = window.matchMedia('(max-width: 1280px)');
+
 function go() {
   // canvas
   canvas = document.getElementById("canvas");
   context = canvas.getContext("2d");
   canvas.width = document.documentElement.clientWidth;
   canvas.height = document.documentElement.clientHeight;
-  if (minDesktop.matches) {
-    brickWidth = brickWidth++;
-  } else {
-    brickWidth = 117;
-  }
+  minLap.matches ? brickWidth = Math.floor(window.innerWidth/8.3): brickWidth = 117;
+  console.log(brickWidth++)
   bricksetup(); 
   bricks(context, brickWidth, brickHeight);
 }
